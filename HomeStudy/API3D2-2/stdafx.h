24ebf842efb955d0cd88c6cde61ca80e7c909fe9
MyeungJunLee <mong356@naver.com>
2020-09-25 11:53:34 +0900

    save

---
+++ HomeStudy/API3D2-2/stdafx.h
@@ -0,0 +1,41 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+#ifdef UNICODE
+#pragma comment(linker, "/entry:wWinMainCRTStartup /subsystem:console")
+#else
+#pragma comment(linker, "/entry:WinMainCRTStartup /subsystem:console")
+#endif
+#include "targetver.h"
+
+#define M_PI 3.14159782
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Converts degrees to radians.
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
+
+// Converts radians to degrees.
+#define radiansToDegrees(angleRadians) (angleRadians * 180.0 / M_PI)
+// Windows Header Files:
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include "cVector3.h"
+#include "cMatrix.h"
+
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+extern HWND g_hWnd;
+
+
+
+// TODO: reference additional headers your program requires here
