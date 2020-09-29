9d6a9dd3ec52474a1e51fca874e7a239b9c16067
MyeungJunLee <mong356@naver.com>
2020-09-29 09:06:31 +0900

    no message

--- HomeStudy/Dxstudy2/cPlayer.cpp
+++ HomeStudy/Dxstudy2/cPlayer.cpp
@@ -1,16 +1,6 @@
 #include "stdafx.h"
 #include "cPlayer.h"
 #include "cCubePC.h"
-<<<<<<< HEAD
-
-
-cPlayer::cPlayer():m_vDirection(0, 0, 1), m_vPosition(0, 0, 0), m_fRotY(0.0f)
-{
-	D3DXMatrixIdentity(&m_matWorld);
-	D3DXMatrixIdentity(&m_matS);
-	D3DXMatrixIdentity(&m_matR);
-	D3DXMatrixIdentity(&m_matT);
-=======
 #include "cAnimation.h"
 
 cPlayer::cPlayer():m_fRotY(0)
@@ -18,66 +8,15 @@ cPlayer::cPlayer():m_fRotY(0)
 	m_vPosition = D3DXVECTOR3(0, 0, 0);
 	m_vDirection = D3DXVECTOR3(0, 0, 1);
 
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 }
 
 
 cPlayer::~cPlayer()
 {
-<<<<<<< HEAD
-	
-=======
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 }
 
 void cPlayer::Setup()
 {
-<<<<<<< HEAD
-
-	
-	D3DXVECTOR3 v;
-
-	
-	cCubePC* cHead = new cCubePC;
-	cHead->Setup(0.6f, 0.6f, 0.6f);
-	v = D3DXVECTOR3(0, 1.4f, 0);
-	cHead->setPosition(v);
-	playerBody.push_back(cHead);
-	
-	cCubePC* cBody = new cCubePC;
-	cBody->Setup(0.6f, 0.7f, 0.3f);
-	v = D3DXVECTOR3(0, 0.73f, 0);
-	cBody->setPosition(v);
-	playerBody.push_back(cBody);
-
-	cCubePC* cRightArm = new cCubePC;
-	cRightArm->Setup(0.3f, 0.7f, 0.3f);
-	v = D3DXVECTOR3(0.48f, 0.73f, 0);
-	cRightArm->setPosition(v);
-	playerBody.push_back(cRightArm);
-
-	cCubePC* cLeftArm = new cCubePC;
-	cLeftArm->Setup(0.35f, 0.7f, 0.3f);
-	v = D3DXVECTOR3(-0.48f, 0.73f, 0);
-	cLeftArm->setPosition(v);
-	playerBody.push_back(cLeftArm);
-
-	cCubePC* cLeftLeg = new cCubePC;
-	cLeftLeg->Setup(0.3f, 0.4f, 0.3f);
-	v = D3DXVECTOR3(-0.15f, 0.2f, 0);
-	cLeftLeg->setPosition(v);
-	playerBody.push_back(cLeftLeg);
-
-	cCubePC* cRightLeg = new cCubePC;
-	cRightLeg->Setup(0.3f, 0.4f, 0.3f);
-	v = D3DXVECTOR3(0.15f, 0.2f, 0);
-	cRightLeg->setPosition(v);
-	playerBody.push_back(cRightLeg);
-	
-
-	m_vPosition = cBody->GetPosition();
-	
-=======
 	D3DXVECTOR3 p,s;
 
 	cCubePC* body = new cCubePC;
@@ -130,49 +69,10 @@ void cPlayer::Setup()
 	m_Animation.Setup(m_partsAnimaiton);
 
 	m_vPosition.y = 0.8f + 0.7f;
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 }
 
 void cPlayer::Update()
 {
-<<<<<<< HEAD
-		//m_Tmat = playerBody[1]->GetTmat();
-		
-		//D3DXMATRIXA16 rotYMat;
-		//D3DXMatrixRotationY(&rotYMat, rotY);*/
-
-		//for each(auto p in playerBody)
-		//{
-		//	//D3DXVECTOR3 newPos = p->GetPosition();
-		//	//D3DXVec3TransformNormal(&newPos, &newPos, &rotYMat);
-		//	//p->setPosition(newPos);
-
-		//	p->setTmat(m_Tmat);
-		//	p->Update();
-		//}
-
-	m_vPosition = playerBody[1]->GetPosition();
-	float rotY = playerBody[1]->m_fRotY;
-	
-	D3DXMATRIXA16 matR, matT;
-	D3DXMatrixRotationY(&matR, rotY);
-
-	m_vDirection = D3DXVECTOR3(0, 0, 1);
-	D3DXVec3TransformNormal(&m_vDirection, &m_vDirection, &matR);
-	
-	D3DXMatrixTranslation(&matT, m_vPosition.x, m_vPosition.y, m_vPosition.z);
-	m_matWorld = matR * matT;
-
-	for each(auto p in playerBody)
-	{
-		p->Update(m_matWorld);
-		//p->setTmat(m_Tmat);
-	}
-		
-	
-	
-		
-=======
 	Input();
 	D3DXMATRIXA16 matT ,matR, matWorld;
 	D3DXQUATERNION quarRot;
@@ -193,30 +93,19 @@ void cPlayer::Update()
 		p->Update(matWorld);
 
 	
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 }
 
 void cPlayer::Render()
 {
-<<<<<<< HEAD
-	for each(auto p in playerBody)
-		p->Render();
-}
-
-D3DXVECTOR3& cPlayer::GetPosition()
-=======
 	for each(auto p in m_vParts)
 		p->Render();
 }
 
 D3DXVECTOR3 & cPlayer::GetPosition()
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 {
 	return m_vPosition;
 }
 
-<<<<<<< HEAD
-=======
 void cPlayer::Input()
 {
 
@@ -257,4 +146,3 @@ void cPlayer::InputProcess(char In)
 		break;
 	}
 }
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
--- HomeStudy/Dxstudy2/cPlayer.h
+++ HomeStudy/Dxstudy2/cPlayer.h
@@ -1,26 +1,11 @@
 #pragma once
-<<<<<<< HEAD
-class cCubePC;
-=======
 #include "cAnimation.h"
 class cCubePC;
 class cAnimation;
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 
 class cPlayer
 {
 private:
-<<<<<<< HEAD
-	float             m_fRotY;
-	D3DXVECTOR3       m_vDirection;
-	D3DXVECTOR3       m_vPosition;
-	D3DXMATRIXA16     m_matWorld;
-	D3DXMATRIXA16     m_matS;
-	D3DXMATRIXA16     m_matR;
-	D3DXMATRIXA16     m_matT;
-
-	vector<cCubePC*> playerBody;
-=======
 	vector<cCubePC*>		m_vParts;
 
 	FLOAT					m_fRotY;
@@ -31,7 +16,6 @@ private:
 	D3DXVECTOR3*			m_PlayerBody[6];
 	vector<D3DXVECTOR3*>	m_partsAnimaiton;
 
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 public:
 	cPlayer();
 	~cPlayer();
@@ -39,17 +23,10 @@ public:
 	void Setup();
 	void Update();
 	void Render();
-<<<<<<< HEAD
-
-public:
-	D3DXVECTOR3&      GetPosition();
-	
-=======
 	D3DXVECTOR3&      GetPosition();
 
 	void Input();
 	void InputProcess(char In);
 
->>>>>>> 3e1ee1aadc6623cb81b24a7f83d252cef31695d3
 };
 
