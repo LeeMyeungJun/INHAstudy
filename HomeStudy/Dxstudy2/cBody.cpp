3119fc22911bc9f3281d58455f86acb6c3a8de91
MyeungJunLee <mong356@naver.com>
2020-09-29 17:51:26 +0900

    no message

--- HomeStudy/Dxstudy2/cBody.cpp
+++ HomeStudy/Dxstudy2/cBody.cpp
@@ -18,9 +18,64 @@ void cBody::Setup()
 	D3DXMatrixScaling(&matS, 0.4f, 0.6f, 0.2f);
 	D3DXMatrixTranslation(&matT, 0.0f, 0.0f, 0.0f);
 	mat = matS * matT;
+
+
+	{
+
+		//front
+		m_vecVertex[0].t = D3DXVECTOR2(20.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[1].t = D3DXVECTOR2(20.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[2].t = D3DXVECTOR2(28.0f / 64.0f, 20.0f / 32.0f);
+
+		m_vecVertex[3].t = D3DXVECTOR2(20.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[4].t = D3DXVECTOR2(28.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[5].t = D3DXVECTOR2(28.0f / 64.0f, 32.0f / 32.0f);
+
+		//back
+		m_vecVertex[6].t = D3DXVECTOR2(32.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[7].t = D3DXVECTOR2(40.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[8].t = D3DXVECTOR2(32.0f / 64.0f, 20.0f / 32.0f);
+
+		m_vecVertex[9].t = D3DXVECTOR2(32.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[10].t = D3DXVECTOR2(40.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[11].t = D3DXVECTOR2(40.0f / 64.0f, 20.0f / 32.0f);
+
+		//left
+		m_vecVertex[12].t = D3DXVECTOR2(16.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[13].t = D3DXVECTOR2(16.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[14].t = D3DXVECTOR2(20.0f / 64.0f, 20.0f / 32.0f);
+
+		m_vecVertex[15].t = D3DXVECTOR2(16.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[16].t = D3DXVECTOR2(20.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[17].t = D3DXVECTOR2(20.0f / 64.0f, 32.0f / 32.0f);
+
+		//right
+		m_vecVertex[18].t = D3DXVECTOR2(28.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[19].t = D3DXVECTOR2(32.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[20].t = D3DXVECTOR2(28.0f / 64.0f, 20.0f / 32.0f);
+
+		m_vecVertex[21].t = D3DXVECTOR2(28.0f / 64.0f, 32.0f / 32.0f);
+		m_vecVertex[22].t = D3DXVECTOR2(32.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[23].t = D3DXVECTOR2(32.0f / 64.0f, 32.0f / 32.0f);
+
+		//top
+		m_vecVertex[24].t = D3DXVECTOR2(20.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[25].t = D3DXVECTOR2(20.0f / 64.0f, 16.0f / 32.0f);
+		m_vecVertex[26].t = D3DXVECTOR2(28.0f / 64.0f, 16.0f / 32.0f);
+
+		m_vecVertex[27].t = D3DXVECTOR2(20.0f / 64.0f, 20.0f / 32.0f);
+		m_vecVertex[28].t = D3DXVECTOR2(28.0f / 64.0f, 16.0f / 32.0f);
+		m_vecVertex[29].t = D3DXVECTOR2(28.0f / 64.0f, 20.0f / 32.0f);
+
+	}
+
+	
 	for(size_t i = 0; i<m_vecVertex.size(); i++)
 	{
 		D3DXVec3TransformCoord(&m_vecVertex[i].p, &m_vecVertex[i].p, &mat);
 	}
+	
+
+	
 	
 }
--- HomeStudy/Dxstudy2/cCubeMan.h
+++ HomeStudy/Dxstudy2/cCubeMan.h
@@ -11,6 +11,10 @@ public:
 protected:
 	cCubeNode*		m_pRoot;
 	D3DMATERIAL9	m_stMtl;
+
+private:
+	LPDIRECT3DTEXTURE9		m_pTexture;
+	
 public:
 	virtual ~cCubeMan();
 
