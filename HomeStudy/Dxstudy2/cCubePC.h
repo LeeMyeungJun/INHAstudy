01fff8d99bee422c35cb2f073f8f7d9388496164
MyeungJunLee <mong356@naver.com>
2020-10-02 13:19:07 +0900

    no message

--- HomeStudy/Dxstudy2/cCubePC.h
+++ HomeStudy/Dxstudy2/cCubePC.h
@@ -6,19 +6,16 @@ public:
 	~cCubePC();
 private:
 	vector<ST_PC_VERTEX> m_vecVertex;
+	
+	float             m_fRotY;
+	D3DXVECTOR3           m_vDirection;
+	D3DXVECTOR3          m_vPosition;
 	D3DXMATRIXA16       m_matWorld;
-	D3DXMATRIXA16		m_matS;
-	FLOAT				m_fRotX; //팔애니메이션 용
-	D3DXVECTOR3			m_vPosition;
+
 public:
-	void Setup(D3DXVECTOR3 position);
-	void Update(D3DXMATRIXA16& matWorld, FLOAT m_fRotY);
+	void Setup();
+	void Update();
 	void Render();
-
-
-	void setScale(D3DXVECTOR3 s);
-
-
-
+	D3DXVECTOR3&      GetPosition();
 
 };
